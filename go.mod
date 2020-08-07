84ffc111d5c2dd792200c1a6bc5dd6e3373619f9
tomaszmartin <tomasz.martin@gmail.com>
2020-08-07 21:03:10 +0200

    Remove Cobra

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/tomaszmartin/gockify
 
 go 1.13
-
-require github.com/spf13/cobra v1.0.0
--- main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/tomaszmartin/gockify/cmd"
-
-func main() {
-	cmd.Execute()
-}
